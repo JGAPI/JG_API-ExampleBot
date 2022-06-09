dd22eab92f66eaa13c6612f5947ced5b401a4407
Nathaniel M <nmoratto629@gmail.com>
2022-06-08 22:38:23 -0700

    Catch errors at Main Class level.

--- src/main/java/dev/JGAPI/ExampleBot/Bot.java
+++ src/main/java/dev/JGAPI/ExampleBot/Bot.java
@@ -1,18 +1,23 @@
 package dev.JGAPI.ExampleBot;
 
 import dev.JGAPI.ExampleBot.Config.ConfigManager;
-import dev.JGAPI.JG_API.Exceptions.ClientBuildException;
 import dev.JGAPI.JG_API.JG_API;
 
 public class Bot {
+    static JG_API jg_api;
 
-    public static void main(String[] args) throws ClientBuildException {
-        JG_API jg_api = new JG_API.ClientBuilder()
-                .setParentServerId(ConfigManager.getInstance().getProperty("GUILDED_SERVER_ID"))
-                .setToken(ConfigManager.getInstance().getProperty("GUILDED_TOKEN"))
-                .build();
+    public static void main(String[] args) {
+        try {
+            jg_api = new JG_API.ClientBuilder()
+                    .setParentServerId(ConfigManager.getInstance().getProperty("GUILDED_SERVER_ID"))
+                    .setToken(ConfigManager.getInstance().getProperty("GUILDED_TOKEN"))
+                    .build();
 
-        jg_api.login();
-        jg_api.start();
+            jg_api.login();
+            jg_api.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
